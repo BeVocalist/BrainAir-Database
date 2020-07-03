c8b48cd875a59af70c958f89518d11ccd7bf738e
Jeremy Yanggit <jeremyinsweden@gmail.com>
2020-07-03 08:10:14 +0200

    New Correction on 14:10,2020.07.03

--- lab4.sql
+++ lab4.sql
@@ -1,59 +1,61 @@
 
 SOURCE BrianAir_schema.sql;
 
-delimiter//
+/*delimiter//
 DROP PROCEDURE IF EXISTS addYear;
 DROP PROCEDURE IF EXISTS addDay;
 DROP PROCEDURE IF EXISTS addDestination;
 DROP PROCEDURE IF EXISTS addRoute;
 DROP PROCEDURE IF EXISTS addFlight;
 
-delimiter ;
+delimiter ;*/
 
-
-delimiter//
+delimiter //
 CREATE PROCEDURE addYear(IN year INT,IN factor FLOAT)
 BEGIN
 
 INSERT INTO Year(year,yearfactor)
-VALUES(year,factor)
+VALUES(year,factor);
 
 END//
 
 delimiter ;
+GO;
 
 delimiter//
 CREATE PROCEDURE addDay(IN year INT,IN day VARCHAR(10),IN factor FLOAT)
 BEGIN
 
-INSERT INTO Weekday(year,day,factor);
+INSERT INTO Weekday(year,day,factor)
 VALUES(year,day,factor);
 
 END//
 
 delimiter ;
+GO;
 
 delimiter //
 CREATE PROCEDURE addDestination(IN airportcode VARCHAR(3), IN name VARCHAR(30), IN country VARCHAR(30))
 BEGIN
 
 INSERT INTO Destination(airport_code,airport_name,country)
-VALUES(airportcode,name,country)
+VALUES(airportcode,name,country);
 
 END//
 delimiter ;
-
+GO;
 
 delimiter//
 CREATE PROCEDURE addRoute(IN departureairportcode VARCHAR(3), IN arrivalairportcode VARCHAR(3),IN year int, IN day varchar(10), IN routeprice float)
 BEGIN
 
 INSERT INTO Route(departure_airport_code,arrival_airport_code,year,day,route_price)
-VALUES(departureairportcode,arrivalairportcode,year,day,routeprice)
+VALUES(departureairportcode,arrivalairportcode,year,day,routeprice);
 
 END//
 
 delimiter ;
+GO;
 
 delimiter//
 CREATE PROCEDURE addFlight(IN departure_airport_code VARCHAR(3),
@@ -62,13 +64,11 @@ IN arrival_airport_code VARCHAR(3),IN year INT,IN day VARCHAR(10), IN departure_
 BEGIN
 
 INSERT INTO flight(departure_airport_code,arrival_airport_code,year,day,departure_time)
-VALUES(departure_airport_code,arrival_airport_code,year,day,departure_time)
+VALUES(departure_airport_code,arrival_airport_code,year,day,departure_time);
 
 END//
 
-delimiter ;
-
-
-
+delimiter;
+GO;
 
 SOURCE Question3.sql;
